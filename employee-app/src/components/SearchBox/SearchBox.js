8678d9477e64ec7aa340ce9e13acf8e44c631b64
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-08 19:58:37 -0400

    Updated searchBox.js

--- employee-app/src/components/SearchBox/SearchBox.js
+++ employee-app/src/components/SearchBox/SearchBox.js
@@ -6,11 +6,12 @@ function SearchBox ({ handleSearch}) {
     return (
       ///Create a div class fro the searchBox/////
       <div className="searchBox">
-          <form className='form'>
+          <form className='form-group'>
+              <label htmlFor="Search">Search</label>
               <input
               className="form-control"
               type="search"
-              Placeholder="Search"
+              placeholder="Search"
               onChnage={e=> handleSearch(e)}
               ///Use a handleSearch method to make sure when the data gets tyoed and submitted that it will return the data retrieved from the API
               />
