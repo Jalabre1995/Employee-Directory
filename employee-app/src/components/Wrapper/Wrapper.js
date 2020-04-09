0516867a0395ef6fc59ec80c8252e119d01fc41a
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-08 20:04:20 -0400

    Added a div className for Wrapper.js

---
+++ employee-app/src/components/Wrapper/Wrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Wrapper/style.css';
+
+function Wrapper({employees}){
+return(
+    <div className ="wrapper">
+        Wrapper
+    </div>
+)
+}
+
+export default Wrapper;
